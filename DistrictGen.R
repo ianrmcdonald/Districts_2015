2b8023ab3ee14c24deaf13918b834478e16bf287
Ian McDonald <ianrmcdonald@gmail.com>
2014-12-16 16:28:50 -0800

    Brutal December 16

--- DistrictGen.R
+++ DistrictGen.R
@@ -35,7 +35,10 @@ step1 <- function() {
         v.mult_memb_states <<- as.factor(c("AZ","NJ","SD","WA"))  ## multi member district states
         v.all.states <<- unique(df.tw.lower$abb)
         v.non_mult_memb_states <<- v.all.states[!(v.all.states %in% v.mult_memb_states)]
-        v.exclude <<- c("ID","VT","NH","MD")
+        v.exclude <<- c("VT","NH","MD","MA")
+        
+        ## DEBUGGING
+        #v.exclude <<- v.all.states[v.all.states != "MN"]
 }
 step1()
 
@@ -50,12 +53,12 @@ step1()
 ####################################################################################################
 
 df.fips <- read.csv("fips.csv")
-df.pop2000.lower <- read.csv("pop2000.csv")      ## total population by state for 2000
+df.pop2000.lower <- read.csv("pop2000.csv",stringsAsFactors=FALSE)      ## total population by state for 2000
 df.pop2000.upper <- df.pop2000.lower                ## clone the pop.2000 file and use for upper houses
-df.sld2010 <- read.csv("lower2010.csv")    ## population by lower house district 2010 ACS
-df.sld2010$ssd_fips <- paste(df.sld2010$fips,df.sld2010$district,sep="")
-df.ssd2010 <- read.csv("upper2010.csv") #  # population by upper house district 2010 ACS
-df.ssd2010$fips_ssd_fips <- paste(df.ssd2010$fips,df.ssd2010$district,sep="")
+df.sld2010 <- read.csv("lower2010.csv",stringsAsFactors=FALSE)    ## population by lower house district 2010 ACS
+df.sld2010$sld_fips <- paste(df.sld2010$fips,df.sld2010$district,sep="")
+df.ssd2010 <- read.csv("upper2010.csv",stringsAsFactors=FALSE) #  # population by upper house district 2010 ACS
+df.ssd2010$ssd_fips <- paste(df.ssd2010$fips,df.ssd2010$district,sep="")
 
 ## determine number of districts per state and use as baseline in population growth percentage calculation
 ## house
@@ -72,7 +75,7 @@ df.pop2000.lower$pd <- round(df.pop2000.lower$pop2000 / df.pop2000.lower$distnum
 df.sld2010 <- merge(df.sld2010,df.pop2000.lower,by="stcd",all.x=TRUE)
 df.sld2010$gpct <- (df.sld2010$pop2010 - df.sld2010$pd) / df.sld2010$pd
 df.sld2010$st_hd <- paste(df.sld2010$stcd,df.sld2010$district,sep="")
-
+df.sld2010$st_hd <- paste(df.sld2010$stcd,df.sld2010$district,sep=":")
 # ## these states have issues with district mapping.  Eliminate for now.
 
 df.sld2010EX <- df.sld2010[df.sld2010$stcd %in% v.exclude,]
@@ -90,13 +93,13 @@ df.pop2000.upper$pd <- round(df.pop2000.upper$pop2000 / df.pop2000.upper$distnum
 
 df.ssd2010 <- merge(df.ssd2010,df.pop2000.upper,by="stcd",all.x=TRUE)
 df.ssd2010$gpct <- (df.ssd2010$pop2010 - df.ssd2010$pd) / df.ssd2010$pd
-df.ssd2010$st_hd <- paste(df.ssd2010$stcd,df.ssd2010$district,sep="")
+df.ssd2010$st_sd <- paste(df.ssd2010$stcd,df.ssd2010$district,sep=":")
 
 
 # ## these states have issues with district mapping.  Eliminate for now.
 df.ssd2010EX <- df.ssd2010[df.ssd2010$stcd %in% v.exclude,]
 df.ssd2010 <- df.ssd2010[!df.ssd2010$stcd %in% v.exclude,]
-df.ssd2010 <- df.ssd2010[df.ssd2010$stcd %in% v.all.states,c("st_hd","gpct")]
+df.ssd2010 <- df.ssd2010[df.ssd2010$stcd %in% v.all.states,c("st_sd","gpct")]
 
 
 ####################################################################################################
@@ -114,6 +117,11 @@ load("state legislator scores july 2014.RData") ## data appears in df "x"
 df.leg.scores <- x
 rm(x)
 
+## Edit for exclusions of states
+df.leg.scores.EX <- df.leg.scores[df.leg.scores$st %in% v.exclude,]
+df.leg.scores <- df.leg.scores[!df.leg.scores$st %in% v.exclude,]
+
+
 
 df.tw.lower <- df.tw.lower[df.tw.lower$abb %in% v.all.states,]
 df.tw.upper <- df.tw.upper[df.tw.upper$abb %in% v.all.states,]
@@ -122,9 +130,11 @@ FILTER.MM <- FALSE   ## A semaphore that indicates whether to use only the four 
 ####################################################################################################
 ## 4:  USE CENSUS GAZETTE DATA TO MAP CENSUS TO DISTRICTS
 ####################################################################################################
-df.gazette <- read.csv("2013_Gaz_sldl_national.csv",header=TRUE)
-
+df.gazette <- read.csv("2013_Gaz_sldl_national.csv",stringsAsFactors=FALSE,header=TRUE)
 df.gazette$sm_name_2 <- paste(df.gazette$st,df.gazette$sm_name_1,sep=":")
+
+##  Merge with gazette records
+
 
 
 ####################################################################################################
@@ -139,7 +149,7 @@ n.sc2.init <- 58
 
 
 for (v.ought in 1:10)  {
-        
+        if(v.ought==7) browser()
         ##  set columns in Shor McCarty extract.
         n.hc1 <- n.hc1.init + v.ought - 1
         n.hc2 <- n.hc2.init + v.ought - 1 
@@ -162,9 +172,18 @@ for (v.ought in 1:10)  {
         
         df.senate.20xx <- df.leg.scores[df.leg.scores[[s.currsdcol]] != "NA", c(1:5,n.sc1,n.sc2,n.sc2-4)]
        
-        ## clean up spaces in s.currhcol and s.currscol
-        df.house.20xx[,s.currhcol] <- substr(df.house.20xx[,s.currhcol],1,3)
-        df.senate.20xx[,s.currscol] <- substr(df.senate.20xx[,s.currscol],1,3)
+        ## CASE 1:  Washington State
+        ## Data intermittently includes a "-1" or "-2" at the end of district id, but not always
+        df.house.20xx[df.house.20xx$st=="WA",s.currhdcol] <- substr(df.house.20xx[df.house.20xx$st=="WA",s.currhdcol],1,3)
+        df.senate.20xx[df.senate.20xx$st=="WA",s.currsdcol] <- substr(df.senate.20xx[df.senate.20xx$st=="WA",s.currsdcol],1,3)
+        
+        ## CASE 2:  South Dakota
+        ## Data includes a "-1" or "-2" for districts 26 and 28
+        ## We will consolidate them
+        df.house.20xx[df.house.20xx$st=="SD",s.currhdcol] <- substr(df.house.20xx[df.house.20xx$st=="SD",s.currhdcol],1,3)
+        df.senate.20xx[df.senate.20xx$st=="SD",s.currsdcol] <- substr(df.senate.20xx[df.senate.20xx$st=="SD",s.currsdcol],1,3)
+        df.sld2010[df.sld2010$st_hd =="SD26A","st_hd"] <- "SD026"
+        df.sld2010[df.sld2010$st_hd =="SD28A","st_hd"] <- "SD028"
         
         
         df.house.20xx$first_term <- is.na(df.house.20xx[[s.prevhcol]])
@@ -218,6 +237,7 @@ for (v.ought in 1:10)  {
         df.house.20xx.aa <- df.house.20xx[!df.house.20xx$multmem.flag,]
         df.senate.20xx.mm<- df.senate.20xx[df.senate.20xx$multmem.flag,]
         df.senate.20xx.aa<- df.senate.20xx[!df.senate.20xx$multmem.flag,]
+ 
                 
         if(nrow(df.house.20xx.mm) > 0) {
                 
@@ -281,11 +301,13 @@ for (v.ought in 1:10)  {
         
         
          
-        rm(df.house.20xx.mm,df.senate.20xx.mm,df.house.20xx.aa,df.senate.20xx.aa,df.iter)
+        rm("df.house.20xx.mm","df.senate.20xx.mm","df.house.20xx.aa","df.senate.20xx.aa")
+        if(exists("df.iter")) rm(df.iter)
+        
+        
         df.house.20xx$year <- n.oyear
         df.senate.20xx$year <- n.oyear
 
-        ### NB:  Rename in plyr does not seem to work with reference variables!!
         names(df.house.20xx)[names(df.house.20xx)==s.currhcol] <- "hdistrict"
         names(df.senate.20xx)[names(df.senate.20xx)==s.currscol] <- "sdistrict"       
         v.drops <- c(s.currhcol, s.currscol, s.currhdcol, s.currsdcol, s.prevhcol, s.prevscol, s.prevhdcol, s.prevsdcol, 
@@ -308,28 +330,34 @@ for (v.ought in 1:10)  {
         df.senate.20xx$party[df.senate.20xx$ind==TRUE & df.senate.20xx$np_score >= 0] <- "R"
         df.senate.20xx$party[df.senate.20xx$ind & df.senate.20xx$np_score < 0] <- "D"        
         df.senate.20xx <- merge(df.senate.20xx,df.gazette,by.x="st_sd",by.y="sm_name_2",all.x=TRUE)
-        df.senate.20xx <- merge(df.senate.20xx,df.ssd2010,by.x="st_sd",by.y="st_hd",all.x=TRUE)
+        df.senate.20xx <- merge(df.senate.20xx,df.ssd2010,by.x="st_sd",by.y="st_sd",all.x=TRUE)
        
         m.house.party <- table(df.house.20xx$st_hd,df.house.20xx$party)
         m.senate.party <- table(df.senate.20xx$st_sd,df.senate.20xx$party)
         
         m.house.party <- as.data.frame(cbind(st_hd = rownames(m.house.party),m.house.party))
-        m.house.party$blend <- m.house.party$D==1 & m.house.party$R==1       
+        m.house.party$blend <- as.numeric(m.house.party$D)>=0 & as.numeric(m.house.party$R)>=0       
         df.house.20xx <- merge(df.house.20xx,m.house.party,by="st_hd"); df.house.20xx$D <- NULL; df.house.20xx$R <- NULL
         
-        ################# ASSUME SENATE DISTRICTS NEVER HAS BLEND ######################
+        ################# ASSUME SENATE DISTRICTS NEVER BLEND ######################
         df.senate.20xx$blend <- FALSE
         ######################################################################
-             
-        df.tw.lower$st_hd <- paste(df.tw.lower$abb,sprintf("%03d",df.tw.lower$ssd_fips_num %% 1000),sep="")
-        df.house.20xx <- merge(df.house.20xx,df.tw.lower,by.x="st_hd",by.y="st_hd",all.x=TRUE)
+        
+        
+        
+        df.tw.lower$st_hd <- paste(df.tw.lower$abb,sprintf("%03d",df.tw.lower$ssd_fips_num %% 1000),sep=":")
+        #df.tw.lower <- merge(df.tw.lower, df.gazette, by.x="st_hd",by.y="sm_name_2",all.x=TRUE)
+        
+        ## Create a lookup field for the gazette
+        df.house.20xx <- merge(df.house.20xx,df.tw.lower,by.x="st_hd",by.y="st_hd",all.x=TRUE)  
         df.house.20xx$pres_2008 <- as.numeric(df.house.20xx$pres_2008)
         df.house.20xx$mrp_estimate <- as.numeric(df.house.20xx$mrp_estimate)
         df.house.20xx$mrp_se <- as.numeric(df.house.20xx$mrp_se)
         
         df.tw.upper$st_sd <- paste(df.tw.upper$abb,sprintf("%03d",df.tw.upper$ssd_fips_num %% 1000),sep="")
+        df.tw.upper <- df.tw.upper  ## Need to replicate gazette process for house
         df.tw.upper[df.tw.upper$abb=="AK",]$st_sd <- paste("AK00",str_sub(df.tw.upper[df.tw.upper$abb=="AK",]$ssd_df.fips,-1),sep="")
-        df.senate.20xx <- merge(df.senate.20xx,df.tw.upper,by.x="st_sd",by.y="st_sd",all.x=TRUE)
+        df.senate.20xx <- merge(df.senate.20xx,df.tw.upper,by.x="st_sd",by.y="st_sd",all.x=TRUE)  ## NOT MERGING WITH GAZETTE!!!! (12/16)
         df.senate.20xx$pres_2008 <- as.numeric(df.senate.20xx$pres_2008)
         df.senate.20xx$mrp_estimate <- as.numeric(df.senate.20xx$mrp_estimate)
         df.senate.20xx$mrp_se <- as.numeric(df.senate.20xx$mrp_se)
